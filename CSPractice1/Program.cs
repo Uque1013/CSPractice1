14477e5d8e50c95b5e43a2455145776dd9348813
y2eun <kye6735@naver.com>
2024-06-25 10:09:50 +0900

    #1 12-1 기본 용어

---
+++ CSPractice1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSPractice1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // 컨텍스트 키워드
+            var abc = "Hello World";    // OK
+            // 한줄 주석
+            /*
+            여러줄 주석
+            */
+            /*** XML 주석***/
+        }
+    }
+}
