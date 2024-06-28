d45202dd3867d3c20f9c04eaa82e76e436c382eb
y2eun <kye6735@naver.com>
2024-06-28 10:06:20 +0900

    #3 14-6.  foreach 반복문

--- CSPractice1/Program.cs
+++ CSPractice1/Program.cs
@@ -129,6 +129,13 @@ namespace CSPractice1
                 Console.WriteLine(intArray2[i]);
                 i++;    // 탈출을 위한 변수
             }
+
+            // #3 14-6.  foreach 반복문
+            string[] foreachArray = { "사과", "배", "포도" };
+            foreach (var item in foreachArray)
+            {
+                Console.WriteLine(item);  
+            }
         }
     }
 }
