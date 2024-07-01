f3f91f8c25423d32831cf2b38d74232592833947
y2eun <kye6735@naver.com>
2024-07-01 13:55:10 +0900

    #5 22-4. 메서드 오버로딩

--- CSPractice1/Program.cs
+++ CSPractice1/Program.cs
@@ -18,6 +18,10 @@ namespace CSPractice1
             // Console.WriteLine(instanceVar); 
             return (input >= 0) ? input : -input;
         }
+
+        // 메서드 시그네이처(이름, 매개변수)가 겹쳐서 안됨
+        //public static double Abs(int input) { return 0; }
+        public static double Abs(double input) { return 0; }
     }
 
     class FirstClass
@@ -199,6 +203,12 @@ namespace CSPractice1
             // Console.WriteLine(p1.TAX_RATIO);  // Java는 가능하나 C#은 X
 
             // #5 22-3. 클래스 메서드
+
+            // #5 22-4. 메서드 오버로딩
+            Console.WriteLine(MyMath.Abs(-10)); // Abs(int)
+            Console.WriteLine(MyMath.Abs(10.0)); // Abs(double)
+            Console.WriteLine(MyMath.Abs(-10L)); // Abs(double)
+            Console.WriteLine(MyMath.Abs(3.0f)); // Abs(double)
         }
     }
 }
