3fa64d69e92c894aaf6daa385a46a89630444dca
y2eun <kye6735@naver.com>
2024-07-01 14:11:47 +0900

    #5 22-8. 속성(Property)

--- CSPractice1/Program.cs
+++ CSPractice1/Program.cs
@@ -7,6 +7,25 @@ using System.Threading.Tasks;
 
 namespace CSPractice1
 {
+    class Box
+    {
+        public int width { get; set; }
+        private int height;
+
+        public int Height
+        {
+            get { return height; }
+            set
+            {
+                if (value > 0)
+                {
+                    height = value;
+                }
+                else { throw new Exception(); }
+            }
+        }
+    }
+
     class Sample
     {
         public static int value;
@@ -239,6 +258,12 @@ namespace CSPractice1
             Console.WriteLine("세 번째 위치");
 
             // #5 22-7. 소멸자
+
+            // #5 22-8. 속성(Property)
+            Box b = new Box();
+            b.width = 100;
+            b.Height = 100;
+            // b.Height = -100; // 예외발생
         }
     }
 }
